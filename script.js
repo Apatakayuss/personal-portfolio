e161c5aafcfe19c86bd73fe219a28e0f209cdb7d
apatakayuss <apatakayuss@gmail.com>
2022-09-26 11:29:43 +0100

    trying to expand see more

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+let click = document.querySelector('.click')
+let drop = document.querySelector('.drop')
+let newDrop = document.querySelector('.newDrop')
+seeMore.addEventListener("seeMore", () =>{
+    drop.classList.toggle('newDrop');
+});
+
+console.log(newDrop);
