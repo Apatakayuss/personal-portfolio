5b3d1a6a181ac2b7d4b9a598b9f3c88e25819a78
apatakayuss <apatakayuss@gmail.com>
2022-09-26 11:35:42 +0100

    leave me

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
 
-let click = document.querySelector('.click')
-let list = document.querySelector('.list')
+let click = document.querySelector('.click');
+let list = document.querySelector('.list');
 // let newDrop = document.querySelector('.newDrop')
 seeMore.addEventListener("click", () => {
     drop.classList.toggle('newList');
